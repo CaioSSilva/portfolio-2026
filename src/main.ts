63670b37517458c504820f0a7d263a74ceaec791
CaioSSilva <caiosouzasilva13650@gmail.com>
2026-07-09 02:42:12 -0300

    Habilitando vercel performance metrics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { inject as injectAnalytics } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights'; // Removed '/next' and updated the function name
+
+injectAnalytics();
+injectSpeedInsights();
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
