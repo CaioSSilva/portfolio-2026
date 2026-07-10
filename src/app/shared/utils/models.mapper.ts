259f878d7f23986b352a48559da14cf379c0bbc8
CaioSSilva <caiosouzasilva13650@gmail.com>
2026-07-09 21:02:08 -0300

    Adicionando traduções

--- src/app/shared/utils/models.mapper.ts
+++ src/app/shared/utils/models.mapper.ts
@@ -34,6 +34,30 @@ export const MODELS_MAPPER: Record<string, ModelMetadata> = {
     labelKey: 'interactive.charger.title',
     descriptionKey: 'interactive.charger.desc',
   },
+  Lava_Lamp: {
+    labelKey: 'interactive.lava_lamp.title',
+    descriptionKey: 'interactive.lava_lamp.desc',
+  },
+
+  Rose_Sword: {
+    labelKey: 'interactive.rose_sword.title',
+    descriptionKey: 'interactive.rose_sword.desc',
+  },
+
+  Fifine_K668: {
+    labelKey: 'interactive.fifine_k668.title',
+    descriptionKey: 'interactive.fifine_k668.desc',
+  },
+
+  Sony_A3: {
+    labelKey: 'interactive.sony_a3.title',
+    descriptionKey: 'interactive.sony_a3.desc',
+  },
+
+  PS2: {
+    labelKey: 'interactive.ps2.title',
+    descriptionKey: 'interactive.ps2.desc',
+  },
 };
 
 export function getModelMetadata(meshName: string): ModelMetadata | undefined {
